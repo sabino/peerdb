19f17ff6faf7349b7c5f6f819fa8d5fb9c75db23
Amogh-Bharadwaj <amogh@peerdb.io>
2025-04-03 00:07:37 +0530

    account for excluded column

--- flow/connectors/clickhouse/qrep_avro_sync.go
+++ flow/connectors/clickhouse/qrep_avro_sync.go
@@ -147,6 +147,11 @@ func (s *ClickHouseAvroSyncMethod) SyncQRepRecords(
 	selectedColumnNames := make([]string, 0, len(schema.Fields))
 	insertedColumnNames := make([]string, 0, len(schema.Fields))
 	for _, colName := range schema.GetColumnNames() {
+		for _, excludedColumn := range config.Exclude {
+			if colName == excludedColumn {
+				continue
+			}
+		}
 		selectedColumnNames = append(selectedColumnNames, "`"+columnNameAvroFieldMap[colName]+"`")
 		insertedColumnNames = append(insertedColumnNames, "`"+colName+"`")
 	}
